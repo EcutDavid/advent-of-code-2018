1e1fa763d66c583d813591824530f9c5c9032197
David Guan <davidguandev@gmail.com>
2018-12-15 14:50:27 +1100

    tidy day-4

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Advent of Code 2018
 My solutions for [adventofcode.com/2018](https://adventofcode.com/2018)
 
-## Solutions
-* Day 1: create a hashtable for the F already being created, whenever there is a new one collide with existing one, done.
+## My Solutions
+* Day 1: For problem 2, creating a hashtable for the F already being created, whenever there is a new one collide with existing one, done.
+* Day 2: For problem 2, A brute force O(n ^ 2 * len(word)) solution that for each line, check all the line after it, whether the "diff" counter is equal to 1.
+* Day 3: Because the "claims" all have small width and height, simply create a two-dimensional array, initialize all the slot to 0, and increment the slot based on the claim, the answer can be found.
+* Day 4: Sort the input lines alphabetically, the result would be time increasing order, the answer then can be found by analysing the intersections.
--- day-4/solution.go
+++ day-4/solution.go
@@ -9,75 +9,52 @@ import (
 	"strings"
 )
 
-// type Interface interface {
-//         // Len is the number of elements in the collection.
-//         Len() int
-//         // Less reports whether the element with
-//         // index i should sort before the element with index j.
-//         Less(i, j int) bool
-//         // Swap swaps the elements with indexes i and j.
-//         Swap(i, j int)
-// }
-
 type lineMeta = struct {
-	rank  int64
 	guard int
 	time  int
 }
-type lineMetaList []lineMeta
 
-func (list lineMetaList) Len() int {
-	return len(list)
-}
-func (list lineMetaList) Less(i, j int) bool {
-	return list[i].rank < list[j].rank
-}
-func (list lineMetaList) Swap(i, j int) {
-	list[i], list[j] = list[j], list[i]
-}
-
-func firstChallenge() {
+func parseInput() []lineMeta {
 	scanner := bufio.NewScanner(os.Stdin)
-
-	lines := lineMetaList{}
+	rawLines := []string{}
 	for scanner.Scan() {
-		line := scanner.Text()
+		rawLines = append(rawLines, scanner.Text())
+	}
+	sort.Strings(rawLines)
+	lines := []lineMeta{}
+	for _, line := range rawLines {
 		var t1, t2, t3, t4, t5 int64 = 0, 0, 0, 0, 0
-
 		fmt.Sscanf(line[:19], "[%d-%d-%d %d:%d]", &t1, &t2, &t3, &t4, &t5)
-		rank := t2*3600*1000 + t3*3600 + t4*60 + t5
+
 		if line[19:19+5] == "Guard" {
 			words := strings.Split(line[19:], " ")
 			id, _ := strconv.Atoi(words[1][1:])
 			lines = append(lines, lineMeta{
-				rank:  rank,
 				guard: id,
 			})
 		} else {
 			lines = append(lines, lineMeta{
-				rank:  rank,
 				time:  int(t5),
 				guard: -1,
 			})
 		}
 	}
-	sort.Sort(lines)
-	guards := map[int][][2]int{}
-	curGuard := -1
-	count := 0
-	for i := 0; i < len(lines); i++ {
-		if lines[i].guard > -1 {
-			curGuard = lines[i].guard
-			if count%2 == 1 {
-			}
+	return lines
+}
+
+func firstChallenge(src []lineMeta) {
+	guards, curGuard := map[int][][2]int{}, -1
+	for i := 0; i < len(src); i++ {
+		if src[i].guard > -1 {
+			curGuard = src[i].guard
 			if guards[curGuard] == nil {
 				guards[curGuard] = [][2]int{}
 			}
-			count = 0
 		} else {
-			guards[curGuard], i = append(guards[curGuard], [2]int{lines[i].time, lines[i+1].time}), i+1
+			guards[curGuard], i = append(guards[curGuard], [2]int{src[i].time, src[i+1].time}), i+1
 		}
 	}
+
 	sleepMostID, most := -1, 0
 	for id, v := range guards {
 		time := 0
@@ -88,6 +65,7 @@ func firstChallenge() {
 			most, sleepMostID = time, id
 		}
 	}
+
 	theGuard := guards[sleepMostID]
 	minMap := map[int]int{}
 	for _, T := range theGuard {
@@ -104,49 +82,19 @@ func firstChallenge() {
 	fmt.Println(best * sleepMostID)
 }
 
-func secondChallenge() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	lines := lineMetaList{}
-	for scanner.Scan() {
-		line := scanner.Text()
-		var t1, t2, t3, t4, t5 int64 = 0, 0, 0, 0, 0
-
-		fmt.Sscanf(line[:19], "[%d-%d-%d %d:%d]", &t1, &t2, &t3, &t4, &t5)
-		rank := t2*3600*1000 + t3*3600 + t4*60 + t5
-		if line[19:19+5] == "Guard" {
-			words := strings.Split(line[19:], " ")
-			id, _ := strconv.Atoi(words[1][1:])
-			lines = append(lines, lineMeta{
-				rank:  rank,
-				guard: id,
-			})
-		} else {
-			lines = append(lines, lineMeta{
-				rank:  rank,
-				time:  int(t5),
-				guard: -1,
-			})
-		}
-	}
-	sort.Sort(lines)
+func secondChallenge(src []lineMeta) {
 	guards := map[int][][2]int{}
 	curGuard := -1
-	count := 0
-	for i := 0; i < len(lines); i++ {
-		if lines[i].guard > -1 {
-			curGuard = lines[i].guard
-			if count%2 == 1 {
-			}
+	for i := 0; i < len(src); i++ {
+		if src[i].guard > -1 {
+			curGuard = src[i].guard
 			if guards[curGuard] == nil {
 				guards[curGuard] = [][2]int{}
 			}
-			count = 0
 		} else {
-			guards[curGuard], i = append(guards[curGuard], [2]int{lines[i].time, lines[i+1].time}), i+1
+			guards[curGuard], i = append(guards[curGuard], [2]int{src[i].time, src[i+1].time}), i+1
 		}
 	}
-	// Each guard, produce: [id, time, count]
 	minCountSlice := [][3]int{}
 
 	for id, g := range guards {
@@ -175,9 +123,10 @@ func secondChallenge() {
 }
 
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "2" {
-		secondChallenge()
-	} else {
-		firstChallenge()
-	}
+	input := parseInput()
+	fmt.Println("first challenge:")
+	firstChallenge(input)
+	fmt.Println("****************")
+	fmt.Println("second challenge:")
+	secondChallenge(input)
 }
